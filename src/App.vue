f1242340da356a006adc8424a5d45dc47a91f0a4
Gillian Shanks <GillianShanks88@gmail.com>
2019-08-28 17:06:41 +0100

    adds working dropdown menu

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,22 @@
   <div class="">
     <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Click me, Kyle</a>
     <h1>Country Navigator</h1>
-    <countries-list :countries="countries"></countries-list>
+    <div class="container">
+      <countries-dropdown :countries="countries"></countries-dropdown>
+
+      <countries-list :countries="countries"></countries-list>
+      <country-detail  :country="selectedCountry"></country-detail>
+    </div>
+
   </div>
-  
+
 </template>
 
 <script>
 import CountriesList from './components/CountriesList.vue';
+import CountryDetail from './components/CountryDetail.vue';
+import CountriesDropdown from './components/CountriesDropdown.vue';
+import {eventBus} from './main.js';
 
 export default {
   name: "app",
@@ -22,9 +31,23 @@ export default {
     fetch('https://restcountries.eu/rest/v2/all')
     .then(response => response.json())
     .then(countries => this.countries=countries)
+
+    eventBus.$on('country-selected', (country) => {
+      this.selectedCountry = country;
+    })
+  },
+  components: {
+    "countries-list": CountriesList,
+    "country-detail": CountryDetail,
+    "countries-dropdown": CountriesDropdown
   }
 }
 </script>
 
 <style lang="css" scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+}
+
 </style>
---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="" v-if="country">
+    <h3>{{country.name}}</h3>
+    <p>{{country.region}}</p>
+    <p>{{country.capital}}</p>
+    <p>{{country.popular}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "country-detail",
+  props: ['country']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
